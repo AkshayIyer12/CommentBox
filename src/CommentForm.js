8a5e1e5ff1d0ab2a863bc241185a4e6cbea4c77d
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-02-25 12:05:42 +0530

    Changed the render component in the CommentForm.js file

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -21,4 +21,24 @@ class CommentForm extends Component {
 		console.log(`${this.state.author} said ${this.state.text}`);
 		//will be trying this into the POST method in a bit
 	}
+	render() {
+		return (
+			<form style={ style.commentForm } onSubmit = { this.handleSubmit }>
+			<input type='text' placeholder='Your name...' 
+			style={ style.commentAuthorForm }
+			value={ this.state.author }
+			onChange={ this.handleAuthorChange } />
+			<input type='text' placeholder='Say something...'
+			style={ style.commentFormText }
+			value={ this.state.text }
+			onChange={ this.handleTextChange } />
+			<input type='submit'
+			style={ style.commentFormPost }
+			value='Post' />
+			</form>
+
+		)
+	}
 }
+
+export default CommentForm;
